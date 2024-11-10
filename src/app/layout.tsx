8f953a6d1fd71ee11bdf8bd69c61058bd31260e8
Deepak S <s.deepak73333@gmail.com>
2024-11-10 20:29:00 +0530

    feat: added lottie animations.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-r from-[#30cfd0] to-[#330867] h-screen`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
+import Player from "react-lottie-player";
+import animationData from "../../public/Animation.json";
 
 export default function Home() {
-  
-  const router = useRouter()
+  const router = useRouter();
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -13,10 +14,21 @@ export default function Home() {
 
   return (
     <div className="flex justify-center flex-col items-center mt-32 ">
-    <h1 className="text-7xl">Hello! I am B-Master</h1>
-    <h3 className="text-4xl mt-3">Detailed Project Report Generator</h3>
-    <input className="w-[40%] outline-none p-3 rounded-full mt-5 text-black
-     placeholder:text-gray-500" type="text" placeholder="Press Enter to Begin" readOnly onKeyDown={handleKeyDown}/>
+      <h1 className="text-7xl">Hello! I am Credit DPR</h1>
+      <h3 className="text-4xl mt-3">Detailed Project Report Generator</h3>
+      <Player
+        play 
+        loop
+        animationData={animationData} 
+        style={{ height: '300px', width: '300px' }}
+      />
+      <input
+        className="w-[40%] outline-none p-3 rounded-full mt-5 text-black placeholder:text-gray-500"
+        type="text"
+        placeholder="Press Enter to Begin"
+        readOnly
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 }
